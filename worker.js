c6f659b5cd4f29bf49d047bae53e83ba6b5f0195
khetsan188-star <khetsan188@gmail.com>
2026-09-19 09:09:35 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -50,7 +50,7 @@ async function hashPassword(password, salt) {
     {
       name: "PBKDF2",
       salt,
-      iterations: 120000,
+      iterations: 100000,
       hash: "SHA-256"
     },
     key,
